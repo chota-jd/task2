f438b35a8affe1c7ee7e9f8e7bff4b91f4c2e5e9
Chirag Prajapati <chotajd4444@gmail.com>
2024-04-09 19:06:48 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+
+  const handleCountryChange = (e) => {
+    setCountry(e.target.value);
+    setState("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select onChange={handleCountryChange} value={country}>
+        <option value="">Select Country</option>
+        <option value="India">India</option>
+        <option value="USA">USA</option>
+        <option value="Germany">Germany</option>
+      </select>
+
+
+      
+      <select value={state} disabled={country === ""}>
+        <option value="">State</option>
+        {country === "India" && (
+          <><option key="Delhi">Delhi</option><option key="Gujrat">Gujrat</option><option key="Himachal pradesh">Himachal pradesh</option><option key="Uttarakhand">Uttarakhand</option><option key="Rajashthan">Rajashthan</option></>
+          
+        )}
+        {country === "USA" && (
+          <><option key="California">California</option><option key="Texas">Texas</option><option key="New York">New York</option></>
+        )}
+        {country === "Germany" && (
+          <><option key="Berlin">Berlin</option><option key="Bavaria">Bavaria</option><option key="Birmingham">Birmingham</option></>
+        )}
+      </select>
     </div>
   );
-}
-
+};
 export default App;
